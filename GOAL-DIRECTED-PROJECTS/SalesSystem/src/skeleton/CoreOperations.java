6e07bf29643a3b9434daec9bc31bf0915b4128fa
JeffBuloto <114021308+JeffBuloto@users.noreply.github.com>
2025-05-17 18:39:45 -0300

    Update CoreOperations.java

--- GOAL-DIRECTED-PROJECTS/SalesSystem/src/skeleton/CoreOperations.java
+++ GOAL-DIRECTED-PROJECTS/SalesSystem/src/skeleton/CoreOperations.java
@@ -3,8 +3,32 @@ package skeleton;
 public class CoreOperations {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int Apple = 8000;
+		int Samsung = 7000;
+		int Dell = 5000;
+		int Acer = 4000;
+		int Asus = 3000;
+
+
+	if (Apple + Samsung > 16000);
+		System.out.println("Congratulations!"  +  " You got 0% off on your purchase");
+	if (Apple + Samsung <= 16000);			
+		System.out.println("Add more items to the cart from the selected brands to pay less");
+	
+	if (Dell + Acer > 10000);
+		System.out.println("Congratulations!"  +  " You got 30% off on your purchase");
+	if (Dell + Acer <= 10000);			
+		System.out.println("Add more items to the cart from the selected brands to pay less");
+	  
+	if (Dell + Asus > 9000);
+		System.out.println("Congratulations!"  +  " You got 30% off on your purchase");
+	if (Dell + Asus <= 9000);			
+		System.out.println("Add more items to the cart from the selected brands to pay less");
+	  
 	}
+		
+	}
+	
 
 }
