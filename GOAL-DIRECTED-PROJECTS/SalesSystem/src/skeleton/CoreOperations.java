1ae8d54058e15d2652bea3a641554267f01716f2
JeffBuloto <114021308+JeffBuloto@users.noreply.github.com>
2025-05-18 18:40:12 -0300

    Update CoreOperations.java

--- GOAL-DIRECTED-PROJECTS/SalesSystem/src/skeleton/CoreOperations.java
+++ GOAL-DIRECTED-PROJECTS/SalesSystem/src/skeleton/CoreOperations.java
@@ -1,4 +1,7 @@
 package skeleton;
+import java.util.Scanner;
+
+Scanner scanner = new Scanner(System.in);
 
 public class CoreOperations {
 
@@ -9,22 +12,68 @@ public class CoreOperations {
 		int Dell = 5000;
 		int Acer = 4000;
 		int Asus = 3000;
+		
+		double Tota1A = Apple + Samsung
+		double TotalB = Dell + Acer
+		double TotalC = Dell + Asus
+		double TotalD = Dell + Asus
+		
+		String Sig = Assinante
 
 
-	if (Apple + Samsung > 16000);
-		System.out.println("Congratulations!"  +  " You got 0% off on your purchase");
-	if (Apple + Samsung <= 16000);			
-		System.out.println("Add more items to the cart from the selected brands to pay less");
+
+
+System.out.println("Which tech brands most interest you?  ");
+Scanner new = new Scanner(System.in);
+
+System.out.println("How many tech itens would you like to buy?  ");
+Scanner new = new Scanner(System.in);
+
+			
+//=================================================================================================//
+
+			
+	if (Apple + Samsung >= 16000);
+		System.out.println("Congratulations!"  +  " You got 60% off on your purchase");
+	)else(
+		System.out.println("Add Apple products to your cart to get 60% off");
 	
-	if (Dell + Acer > 10000);
-		System.out.println("Congratulations!"  +  " You got 30% off on your purchase");
-	if (Dell + Acer <= 10000);			
-		System.out.println("Add more items to the cart from the selected brands to pay less");
+	if (Dell + Acer >= 10000);
+		System.out.println("Congratulations!"  +  " You got 50% off on your purchase");
+	)else(
+		System.out.println("Add Acer products to your cart to get 40% off");
+	
 	  
-	if (Dell + Asus > 9000);
+	if (Dell + Asus >= 9000);
 		System.out.println("Congratulations!"  +  " You got 30% off on your purchase");
-	if (Dell + Asus <= 9000);			
-		System.out.println("Add more items to the cart from the selected brands to pay less");
+	)else(
+		System.out.println("Add Asus products to your cart to get 20% off");
+
+
+	if boolean (Assinante) = true
+		System.out.println("You have received 60% off on your cart for beeing a member of our subscriptuion plan")
+	)else(
+		System.out.println("Be amember of our subscription club to get 60% off on any purchase")
+
+
+		
+//==================================================================================================//
+
+	System.out.println("Thank you for buying with us, your order is already beeing processed by our delivery team")
+
+	if (double TotalA):
+		System.out.println("The total is" + TotalA)
+	
+	if (double TotalB):
+	        System.out.println("The total is" + TotalB)
+
+	if (double TotalC):
+	        System.out.println("The total is" + TotalC)
+
+	if (double TotalD):
+		System.out.println("The total is" + TotalD)
+	         
+		
 	  
 	}
 		
