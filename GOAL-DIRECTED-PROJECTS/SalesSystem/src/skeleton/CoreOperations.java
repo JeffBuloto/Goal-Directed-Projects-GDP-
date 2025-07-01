a7a634bd127757ff59a789838b86cb0fad434a04
JeffBuloto <114021308+JeffBuloto@users.noreply.github.com>
2025-07-01 20:30:19 -0300

    Update CoreOperations.java

--- GOAL-DIRECTED-PROJECTS/SalesSystem/src/skeleton/CoreOperations.java
+++ GOAL-DIRECTED-PROJECTS/SalesSystem/src/skeleton/CoreOperations.java
@@ -1,79 +1,80 @@
 package skeleton;
+
 import java.util.Scanner;
 
-Scanner scanner = new Scanner(System.in);
-
-public class CoreOperations {
-
-	public static void main(String[] args) {
-
-		int Apple = 8000;
-		int Samsung = 7000;
-		int Dell = 5000;
-		int Acer = 4000;
-		int Asus = 3000;
-		
-		double Tota1A = Apple + Samsung
-		double TotalB = Dell + Acer
-		double TotalC = Dell + Asus
-		double TotalD = Dell + Asus
-		
-		boolean Assinante
 
 
+	public class CoreOperations {
 
+		public static void main(String[] args) {
 
-System.out.println("Which tech brands most interest you?  ");
-Scanner new = new Scanner(System.in);
-
-System.out.println("How many tech itens would you like to buy?  ");
-Scanner new = new Scanner(System.in);
-
+			int Apple = 8000;
+			int Samsung = 7000;
+			int Dell = 5000;
+			int Acer = 4000;
+			int Asus = 3000;
 			
-//=================================================================================================//
-
+			double Total;
+			double TotalA = Apple + Samsung;
+			double TotalB = Dell + Acer;
+			double TotalC = Acer + Asus;
 			
-	if (Apple + Samsung >= 16000);
-		System.out.println("Congratulations!"  +  " You got 60% off on your purchase");
-	}else{
-		System.out.println("Add Apple products to your cart to get 60% off");
+			
+			Scanner scanner = new Scanner(System.in) {
+
+
+				{
+
+	System.out.println("Which tech brands are mostly of your interest?  ");
+	Scanner interest = scannernextLine();
 	
-	if (Dell + Acer >= 10000);
-		System.out.println("Congratulations!"  +  " You got 50% off on your purchase");
-	}else{
-		System.out.println("Add Acer products to your cart to get 40% off");
+
+	System.out.println("How many tech items would you like to buy?  ");
+	int items = scanner.nextInt();
 	
-	  
-	if (Dell + Asus >= 9000);
-		System.out.println("Congratulations!"  +  " You got 30% off on your purchase");
-	}else{
-		System.out.println("Add Asus products to your cart to get 20% off");
+			}
+	
 
+				
+	//=================================================================================================//
 
-	if boolean (Assinante) = true
-		System.out.println("You have received 60% off on your cart for beeing a member of our subscriptuion plan")
-	}else{
-		System.out.println("Be amember of our subscription club to get 60% off on any purchase")
+				
+		if (Apple + Samsung >= 16000);
+			System.out.println("Congratulations!"  +  " You got 60% off on your purchase");
+		
+			System.out.println("Add Apple products to your cart to get 60% off");
+		
+		if (Dell + Acer >= 12000);
+			System.out.println("Congratulations!"  +  " You got 50% off on your purchase");
+		
+		  
+		if (Dell + Asus >= 9000);
+			System.out.println("Congratulations!"  +  " You got 30% off on your purchase");
+		
+		   
+			  boolean Assinante = true;
+			  
+			  if (Assinante) {
+		            System.out.println("You have received 60% off on your cart for being a member of our subscription plan");
+		        } else {
+		            System.out.println("Be a member of our subscription club to get 60% off on any purchase");
+		        }
+	//==================================================================================================//
 
+		  if (Total == TotalA);
+		  System.out.println("The total is" + TotalA);
+			
+		  if (Total == TotalB);
+		  System.out.println("The total is" + TotalB);
+
+		  if (Total == TotalC);
+		  System.out.println("The total is" + TotalC);
 
 		
-//==================================================================================================//
-
-	System.out.println("Thank you for buying with us, your order is already beeing processed by our delivery team")
-
-	if (TotalA):
-		System.out.println("The total is" + TotalA)
+	System.out.println("Thank you for buying with us, your order is already beeing processed by our delivery team");
 	
-	if (TotalB):
-	        System.out.println("The total is" + TotalB)
-
-	if (TotalC):
-	        System.out.println("The total is" + TotalC)
-
-	if (TotalD):
-		System.out.println("The total is" + TotalD)
-	         
-		
+	
+			
 	  
 	}
 		
