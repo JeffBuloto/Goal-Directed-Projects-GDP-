921bf58bccd284d301c0575dc2c70b18cd2631f3
JeffBuloto <114021308+JeffBuloto@users.noreply.github.com>
2025-07-13 18:45:48 -0300

    Update Products.java

--- GOAL-DIRECTED-PROJECTS/SalesSystem/src/skeleton/Products.java
+++ GOAL-DIRECTED-PROJECTS/SalesSystem/src/skeleton/Products.java
@@ -3,7 +3,43 @@ package skeleton;
 public class Products {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+	        String Macbook;
+		String GalaxyBook;
+		String Inspiron15;
+		String VivoBook16;
+		String Alienware;
+                String Predator;
+        
+      Scanner scanner = new Scanner(System.in);
+      
+      
+      System.out.println("Are you interested into laptops for gaming?");
+      Scanner gaming = scanner.reset();
+      
+      System.out.println("Are you interested into laptops for work?");
+      Scanner work = scanner.reset();
+ //====================================================================================================//
+      
+     boolean Gaming = true;
+    		 if (Gaming) {
+    			 System.out.println("We highly recommend you either the" + Alienware + "or the" +  Predator  + "Series");
+    	} else {
+    		System.out.println("Please select another laptop cathegory option");
+    		
+     boolean Work = true;
+             if (Work) {
+            	 System.out.println("We highly recommend you the" + Macbook + "series for design, editing and programming professionals");
+            	 System.out.println("For jobs that require less creative skills, we suggest the" +  GalaxyBook  +  Inspiron15  + "and the Vivobook15 series");
+    
+//====================================================================================================//             
+             System.out.println("Our store has a discount system that is based on the ammount of items on your cart, the correspoding brand and if you are a member of your subscription club");
+             
+             }
+    		 }
+	}
+
+}
 
 	}
 
