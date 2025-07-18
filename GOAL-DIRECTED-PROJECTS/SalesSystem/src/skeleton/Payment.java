6c9249bc4290b4145eb30659fb3645e89d531084
JeffBuloto <114021308+JeffBuloto@users.noreply.github.com>
2025-07-17 22:16:11 -0300

    Add files via upload

---
+++ GOAL-DIRECTED-PROJECTS/SalesSystem/src/skeleton/Payment.java
@@ -0,0 +1,31 @@
+package skeleton;
+
+import java.util.Scanner;
+
+public class Payment {
+
+	public static void main(String[] args) {
+		
+		String PayPal;
+		String BitCoin;
+		String ApplePay;
+		String GooglePay;
+		
+        Scanner scanner = new Scanner(System.in);
+        
+       System.out.println("Which payment method would you like to use?");
+       Scanner use = scanner.reset();
+       
+//=============================================================================//
+       
+       boolean MobilePay = true;
+     
+       
+       
+       
+       
+		
+
+	}
+
+}
