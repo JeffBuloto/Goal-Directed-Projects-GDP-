3ea8656ca5d59e9b083f77746f04d66e7776bccb
JeffBuloto <114021308+JeffBuloto@users.noreply.github.com>
2026-02-02 19:18:36 -0300

    Update Products.java

--- GOAL-DIRECTED-PROJECTS/SalesSystem/src/skeleton/Products.java
+++ GOAL-DIRECTED-PROJECTS/SalesSystem/src/skeleton/Products.java
@@ -1,46 +1,49 @@
 package skeleton;
 
+import java.util.Scanner;
+
 public class Products {
-
+    
 	public static void main(String[] args) {
-
-	        String Macbook;
-		String GalaxyBook;
-		String Inspiron15;
-		String VivoBook16;
-		String Alienware;
-                String Predator;
-        
+                    
+               
+                       
       Scanner scanner = new Scanner(System.in);
+    
       
+      System.out.println("Are you interested into laptops for games?  ");
+      boolean games = scanner.nextBoolean();
       
-      System.out.println("Are you interested into laptops for gaming?");
-      Scanner gaming = scanner.reset();
-      
-      System.out.println("Are you interested into laptops for work?");
-      Scanner work = scanner.reset();
+      System.out.println("Are you interested into laptops for work?  ");
+      boolean work = scanner.nextBoolean();
  //====================================================================================================//
       
-     boolean Gaming = true;
-    		 if (Gaming) {
-    			 System.out.println("We highly recommend you either the" + Alienware + "or the" +  Predator  + "Series");
-    	} else {
-    		System.out.println("Please select another laptop cathegory option");
-    		
-     boolean Work = true;
-             if (Work) {
-            	 System.out.println("We highly recommend you the" + Macbook + "series for design, editing and programming professionals");
-            	 System.out.println("For jobs that require less creative skills, we suggest the" +  GalaxyBook  +  Inspiron15  + "and the Vivobook15 series");
-    
+             if (games) {
+                System.out.println("We highly recommend you either the Alienware or the  Predator Series");
+    	        System.out.println("For games that a modest hardware is enough, we highly recommend you the Nitro series");
+             } else {
+                 System.out.println("Check the laptops we have for labor activities");
+            
+            }	
+     
+             if (work) {
+            	 System.out.println("We highly recommend you the" + "Macbook" + "series for design, editing and programming professionals");
+            	 System.out.println("For jobs that require less creative skills, we suggest the" +  "GalaxyBook"  +  "Inspiron15"  + "and the Vivobook15 series");
+                 System.out.println("Our store has a discount system based on the amount of items in your cart, the brand, and your membership status");
+            
+        }
 //====================================================================================================//             
              System.out.println("Our store has a discount system that is based on the ammount of items on your cart, the correspoding brand and if you are a member of your subscription club");
              
              }
     		 }
-	}
+	 
 
+
+  
 }
 
 	}
 
 }
+
